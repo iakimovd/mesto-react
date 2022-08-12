f97e43669a6acc5cb4b85ef2cc063b2f329a05ce
Dmitrii <dimitry.vs@gmail.com>
2022-08-12 14:23:29 +0500

    fix: change background  overlay value for picture popup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,7 @@ import React from "react";
 export function ImagePopup({ card, onClose }) {
 
   return (
-    <div className={`popup open-picture-popup ${card ? "popup_opened" : ""}`} onClick={onClose}>
+    <div className={`popup open-picture-popup popup_picture-show ${card ? "popup_opened" : ""}`} onClick={onClose}>
       <div className="popup__picture-container" onClick={e => e.stopPropagation()}>
         <img className="popup__image" src={card ? card.link : ""} alt={card ? card.name : ""} />
         <h3 className="popup__picture-description">{card ? card.name : ""}</h3>
